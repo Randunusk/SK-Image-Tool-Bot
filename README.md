499648370a2d47a0c981d07d05cd82d4e8a56072
youtubeslgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-06-24 19:23:26 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# advance-image-tool-bot
+<p align="center"><a href="https://t.me/theimagebot"><img src="https://cdn.dribbble.com/users/21546/screenshots/3998998/42_astrobot.png" width="500"></a></p> 
+<p align='center'>// <a href="https://dribbble.com/Dangerdom">Image Credits</a> //</p>
+
+<h1 align="center">theimagebot</h1>
+
+<h4 align="center">A Telegram Bot Which Can Do Many Things With Image</h4>
+
+<p> ➠ I Can Carbonized Your Code </p>
+<p> ➠ I Can Extract Text From An Image</p>
+<p> ➠ I Can Reverse Search An Image</p>
+<p> ➠ I Can Add Watermark to An Image</p>
+<p> ➠ I Can Blur An Image</p>
+
+## Demo Bot
+
+
+<a href="https://t.me/theimagebot"><img src="https://img.shields.io/badge/Bot%20Status%20-Up-%3CCOLOR%3E"></a>
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+
+## Support
+
+
+<a href="https://t.me/theostrich"><img src="https://img.shields.io/badge/Join-Telegram%20Channel-red.svg?logo=Telegram"></a>
+<a href="https://t.me/ostrichdiscussion"><img src="https://img.shields.io/badge/Join-Telegram%20Group-blue.svg?logo=telegram"></a>
+<a href="https://t.me/no_one_luv_me"><img src="https://img.shields.io/badge/dev-Black"></a>
+
+### Mandatory Configs Vars 📒
+
+```
+
+[-] TOKEN : { You can get this value from https://t.me/BotFather }
+[-] CHROMEDRIVER_PATH : /app/.chromedriver/bin/chromedriver
+[-] GOOGLE_CHROME_BIN : /app/.apt/usr/bin/google-chrome
+[-] TESSDATA_PREFIX : ./.apt/usr/share/tesseract-ocr/4.00/tessdata
+
+```
+
+### Build Pack 🛠
+
+```
+
+[+] heroku/python
+[+] https://github.com/heroku/heroku-buildpack-apt
+[+] https://github.com/heroku/heroku-buildpack-google-chrome
+[+] https://github.com/heroku/heroku-buildpack-chromedriver
+
+```
+
+# Licence
+
+* Copyright (C) 2020-2021 by <a href="https://github.com/NoOneLuvMe">NoOneLuvMe@Github</a>.
+
+[![GNU GPLv3 Image](https://www.gnu.org/graphics/gplv3-127x51.png)](http://www.gnu.org/licenses/gpl-3.0.en.html)  
+
+theimagebot is Free Software: You can use, study share and improve it at your
+will. Specifically you can redistribute and/or modify it under the terms of the
+[GNU General Public License](https://www.gnu.org/licenses/gpl.html) as
+published by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version. 
---
+++ main.py
@@ -0,0 +1,350 @@
+# Repo -> https://github.com/NoOneLuvMe/theimagebot
+# Dev -> t.me/no_one_luv_me
+# channel -> https://t.me/theostrich
+# support -> https://t.me/ostrichdiscussion
+
+import requests
+import logging
+import pytesseract
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+from telegram.ext import Updater, CommandHandler
+from telegram.ext.dispatcher import run_async
+import carbonsh
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def imagetotext(update, context):
+    context.bot.get_file(update.message.reply_to_message.photo[-1]).download(custom_path="./downloads/PictoText.jpg")
+    username = update.message.chat.username
+    print("started By : ", username)
+
+    messa = '''<b>
+Please Wait For Few Minutes 🧘‍♂️
+
+Generating 🍧 Text From The Image 🌠
+
+Please Don't Spam 🥺
+
+Join please @OMG_info🙄
+    </b>'''
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='html')
+    pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
+
+    # Method to process the blue band
+    def pixelProcBlue(intensity):
+        return intensity
+
+    imageObject = Image.open('downloads/PictoText.jpg')
+    multiBands = imageObject.split()
+    blueBand = multiBands[2].point(pixelProcBlue)
+
+    image_to_text = pytesseract.image_to_string(blueBand, lang='eng')
+    image_to_text += '''
+
+Text from Image By @Carbon0_bot ❤
+    '''
+    context.bot.send_message(chat_id=update.effective_chat.id, text=image_to_text)
+
+
+def addwatermark(update, context):
+    text_watermark_from_user = context.args
+    username = update.message.chat.username
+    print("addwatermark By : ", username)
+
+    watermark_text = str(text_watermark_from_user).split(":")[0].strip().replace("'", "").replace(",", "").replace("[",
+                                                                                                                   "").replace(
+        "]", "")
+    size_ = int(
+        str(text_watermark_from_user).split(":")[1].strip().replace("'", "").replace(",", "").replace("[", "").replace(
+            "]", ""))
+
+    if size_ < 100:
+
+        context.bot.get_file(update.message.reply_to_message.photo[-1]).download(
+            custom_path="./downloads/@theimagebot.png")
+        messa = '''<b>
+Please Wait For Few Seconds 🧘‍♂️
+
+Adding Watermark 🍃 to the image 🌠
+
+Please Don't Spam 🥺
+
+Join please @OMG_info☹️
+                </b>'''
+        update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='html')
+
+        photo = Image.open("./downloads/@theimagebot.png")
+        # make the image editable
+        width, height = photo.size
+
+        draw = ImageDraw.Draw(photo)
+        text = watermark_text
+        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', int(size_))
+        textwidth, textheight = draw.textsize(text, font)
+
+        # calculate the x,y coordinates of the text
+        margin = 5
+        x = width - textwidth - margin
+        y = height - textheight - margin
+
+        # draw watermark in the bottom right corner
+        draw.text((x, y), text, font=font)
+        photo.save('./downloads/@theimagebot.png')
+
+        caption = "<b>Watermark Added By <a herf=\"http://t.me/theimagebot\">@theimagebot</a></b>❤️"
+        context.bot.send_document(chat_id=update.effective_chat.id,
+                                  document=open('./downloads/@theimagebot.png', 'rb'),
+                                  caption=caption, parse_mode="html")
+
+    else:
+        messa = '''
+*Size Must Be < 100 😇
+
+Use /help If U don't Know How to Use Me*
+    '''
+        update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='markdown')
+
+
+def blur(update, context):
+    username = update.message.chat.username
+    print("Blured By : ", username)
+    context.bot.get_file(update.message.reply_to_message.photo[-1]).download(
+        custom_path="./downloads/@theimagebot.png")
+    blur_radious = int(context.args[0])
+
+    if blur_radious < 100:
+
+        messa = '''<b>
+Please Wait For Few Seconds 🧘‍♂️
+
+Start Bluring 🧖‍♂️ the image 🌠
+
+Please join @OMG_info 🥺
+                            </b>'''
+
+        update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='html')
+
+        Photo = Image.open("./downloads/@theimagebot.png")
+        photo = Photo.filter(ImageFilter.GaussianBlur(radius=blur_radious))
+        photo.save('./Editing/@theimagebot.png')
+
+        caption = "<b>Blured  By <a herf=\"http://t.me/theimagebot\">@theimagebot</a></b> ❤️"
+        context.bot.send_document(chat_id=update.effective_chat.id,
+                                  document=open('./Editing/@theimagebot.png', 'rb'),
+                                  caption=caption, parse_mode="html")
+
+    else:
+        messa = '''
+*Blur Value Must Be < 100 😇
+
+Use /help If U don't Know How to Use Me*    '''
+        update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='markdown')
+
+
+def reverse(update, context):
+    username = update.message.chat.username
+    print("Reverse By : ", username)
+    context.bot.get_file(update.message.reply_to_message.photo[-1]).download(
+        custom_path="./downloads/@theimagebot.png")
+    messa = '''<b>
+Please Wait For Few Seconds 🧘‍♂️
+
+Reversing 🔍 the image
+
+Please Don't Spam 🥺
+                                    </b>'''
+
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='html')
+
+    filePath = "./downloads/@theimagebot.png"
+    searchUrl = 'http://www.google.hr/searchbyimage/upload'
+    multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+    response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+    fetchUrl = response.headers['Location']
+    link = f'''
+<b>Your Search Result 👇 </b>
+
+{fetchUrl}
+
+<b>Reversed By️
+   <a herf=\"http://t.me/theimagebot\">@theimagebot</a></b> ❤
+'''
+    context.bot.send_message(chat_id=update.effective_chat.id, text=link, parse_mode='html')
+
+
+@run_async
+def start(update, context):
+    username = update.message.chat.username
+    print("started By : ", username)
+    welcome = f'''
+<b>Hey {username} 👋
+
+I'm <a herf=\"http://t.me/theimagebot\">theimagebot</a> 
+
+I'm a Open Source Bot 
+/source_code to Get Repo Link 😌
+
+I Can Do Many Things
+
+💫 ➠ I Can Carbonized A Code 
+💫 ➠ I Can Extract Text From An Image
+💫 ➠ I Can Reverse Search An Image
+💫 ➠ I Can Add Watermark to An Image
+💫 ➠ I Can Blur A Image
+
+Hit /help If You Don't Know How to Use Me 
+
+Developer : <a href=\"t.me/shado_hackers\"> noob poppl </a>🧑‍💻
+
+Support Group : <a href=\"https://t.me/OMG_info\">OMG!nfo</a> 🦸‍♂️
+
+Made By <a href=\"https://t.me/shado_hackers\"> LUSIFER </a> ❤️
+
+</b>
+'''
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=welcome, parse_mode='html',
+                              disable_web_page_preview=True)
+
+@run_async
+def help(update, context):
+    help_ = '''
+*You Don't Know How to Use Me ? ok
+
+To Get text from that image 🏃‍♂️
+
+replay to an image with /imagetotext  
+
+To Reverse search a image 🔍
+
+replay to an image with /reverse
+
+To Carbonized a Code 🌱
+
+replay to a Message with /carbon
+
+To Add watermark to an image 🏋️‍♂
+️
+replay to an image with /addwatermark {text to be watermark}:{watermark size}
+
+example: /addwatermark @OMG_info:30
+
+To Blur A Image 🚵‍♀️
+
+replay to an image with /blur {blur value}
+
+example: /blur 40
+
+I'm a Open Source Bot 
+/source_code to Get Repo Link 😌
+*
+    '''
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=help_, parse_mode='markdown')
+
+
+def carbon(update, context):
+    username = update.message.chat.username
+    print("Carbon By : ", username)
+    messa = '''<b>
+Please Wait For Few Seconds 🧘‍♂️
+
+Carbonizing 🌿 the Your Code 👨‍💻
+
+Please help me to join @OMG_info 🥺
+                                        </b>'''
+
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=messa, parse_mode='html')
+
+    code = update.message.reply_to_message.text
+
+    config = carbonsh.Config(language=carbonsh.languages.AUTO)
+
+    # returns >>> 'https://carbon.now.sh/?bg=rgba(...'
+    carbon_url = carbonsh.code_to_url(code, config)
+
+    options = webdriver.ChromeOptions()
+    prefs = {
+        "download.default_directory": "./Carbon/",
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True
+    }
+
+    options.add_experimental_option('prefs', prefs)
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    driver.get(carbon_url)
+    time.sleep(5)
+    button = driver.find_element_by_xpath("//button[@class='jsx-1730877631 ']")
+    button.click()
+    time.sleep(5)
+    caption = "<b>Carbonized By <a herf=\"http://t.me/theimagebot\">@theimagebot</a></b>❤️"
+    context.bot.send_document(chat_id=update.effective_chat.id,
+                              document=open('./Carbon/carbon.png', 'rb'),
+                              caption=caption, parse_mode="html")
+    driver.close()
+
+
+@run_async
+def source_code(update, context):
+    username = update.message.chat.username
+    print("Source  : ", username)
+    source = '''
+
+Developer : <a href=\"t.me/no_one_luv_me\"> ෴ 乂(≧▽≦) 乂 ෴ </a>🧑‍💻
+    
+Repository Link : https://github.com/NoOneLuvMe/theimagebot
+    
+<b>Give a 🌟 if you liked the repo ❤️</b>
+
+Support Group : <a href=\"https://t.me/ostrichdiscussion\">Ostrich Discussion</a> 🦸‍♂️
+
+Made By <a href=\"https://t.me/theostrich\"> Ostrich </a> ❤️  
+
+
+ 
+    '''
+    update.message.reply_text(reply_to_message_id=update.message.message_id, text=source, parse_mode='html',
+                              disable_web_page_preview=True)
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    start_handler = CommandHandler('start', start, run_async=True)
+    help_handler = CommandHandler('help', help, run_async=True)
+    source_code_handeler = CommandHandler('source_code', source_code, run_async=True)
+    addwatermark_handeler = CommandHandler('addwatermark', addwatermark, run_async=True)
+    imagetotext_handeler = CommandHandler('imagetotext', imagetotext)
+    reverse_handeler = CommandHandler('reverse', reverse)
+    blur_handeler = CommandHandler('blur', blur)
+    carbon_handeler = CommandHandler('carbon', carbon)
+    
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(carbon_handeler)
+    dispatcher.add_handler(reverse_handeler)
+    dispatcher.add_handler(blur_handeler)
+    dispatcher.add_handler(addwatermark_handeler)
+    dispatcher.add_handler(imagetotext_handeler)
+    dispatcher.add_handler(source_code_handeler)
+
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
+# Dev -> t.me/no_one_luv_me
+# channel -> https://t.me/theostrich
+# support -> https://t.me/ostrichdiscussion
